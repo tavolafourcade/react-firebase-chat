e5701513a80c2a686d75a1ebb8e494484c5c83aa
tavolafourcade <tavolafourcade@gmail.com>
2024-06-02 11:41:19 -0500

    Add user modal

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./chatList.css"
+import AddUser from "./addUser/AddUser"
 
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false)
@@ -45,6 +46,7 @@ const ChatList = () => {
           <p>Hello</p>
         </div>
       </div>
+      { addMode ? <AddUser /> : "" }
     </div>
   )
 }
